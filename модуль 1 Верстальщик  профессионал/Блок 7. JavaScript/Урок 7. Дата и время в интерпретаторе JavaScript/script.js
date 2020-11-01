8bbc694a0bb9758fa96cffb492b4bac2c4b0d95b
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-01 21:16:36 +0800

    timer

--- модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 7. Дата и время в интерпретаторе JavaScript/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 7. Дата и время в интерпретаторе JavaScript/script.js
@@ -10,7 +10,7 @@ function NewYear(){
 	var newYear = new Date();
 	newYear.setMonth(11,31);
 	newYear.setHours(0,0,0,0);
-	var year = now.getFullYear();
+	var year = now.getFullYear() + 1;
 	var div = document.getElementById('NewYear');
 	var timer = newYear.getTime() - now.getTime();
 	var days = parseInt(timer/(24*60*60*1000));
@@ -26,16 +26,14 @@ function NewYear(){
 function Mode(){
 	var now = new Date();
 	var open = new Date();
+    
 	open.setHours(8,0,0,0);
 	var close = new Date();
 	close.setHours(20,0,0,0);
-	var midNight = new Date();
-	midNight.setHours(0,0,0,0);
 	var mode = document.getElementById('mode');
-	if(now.getTime()<open.getTime()||now.getTime()>close.getTime()){
-		if(now.getTime()<midNight.getTime())
-			var timer = (midNight.getTime() - now.getTime()) + open.getTime();
-			else var timer = open.getTime() - now.getTime();
+	if(now.getTime()>close.getTime()){
+		open.setDate(open.getDate()+1);
+		var timer = open.getTime() - now.getTime();
 
 		var hours = parseInt(timer/(60*60*1000));
 		if(hours<10) hours = '0'+ hours;
@@ -43,16 +41,29 @@ function Mode(){
 		if(min<10) min = '0'+ min;
 		var sec = parseInt(timer/1000)%60;
 		if(sec<10) sec = '0'+ sec;
-		mode.innerHTML = 'До открытия ' + hours + ' : ' + min + ' : ' + sec;
+		mode.innerHTML = 'До открытия  ' + hours + ' : ' + min + ' : ' + sec;
 	}
-	else  var timer = close.getTime() - now.getTime();
-	var hours = parseInt(timer/(60*60*1000));
-	if(hours<10) hours = '0'+ hours;
-	var min = parseInt(timer/(60*1000))%60;
-	if(min<10) min = '0'+ min;
-	var sec = parseInt(timer/1000)%60;
-	if(sec<10) sec = '0'+ sec;
-	mode.innerHTML = 'До закрытия ' + hours + ' : ' + min + ' : ' + sec;
+	else if(now.getTime()<close.getTime()&&now.getTime()<open.getTime()){
+		var timer = open.getTime() - now.getTime();
+
+		var hours = parseInt(timer/(60*60*1000));
+		if(hours<10) hours = '0'+ hours;
+		var min = parseInt(timer/(60*1000))%60;
+		if(min<10) min = '0'+ min;
+		var sec = parseInt(timer/1000)%60;
+		if(sec<10) sec = '0'+ sec;
+		mode.innerHTML = 'До открытия  ' + hours + ' : ' + min + ' : ' + sec;
+	}
+	else{ 
+		var timer = close.getTime() - now.getTime();
+		var hours = parseInt(timer/(60*60*1000));
+		if(hours<10) hours = '0'+ hours;
+		var min = parseInt(timer/(60*1000))%60;
+		if(min<10) min = '0'+ min;
+		var sec = parseInt(timer/1000)%60;
+		if(sec<10) sec = '0'+ sec;
+		mode.innerHTML = 'До закрытия ' + hours + ' : ' + min + ' : ' + sec;
+	}
 	setTimeout(Mode,1000);
 }
 window.onload = displayTime();
