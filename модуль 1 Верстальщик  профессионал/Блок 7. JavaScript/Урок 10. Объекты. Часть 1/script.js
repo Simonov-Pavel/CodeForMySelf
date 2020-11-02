eb818221a948d522227a4c649a41c1efa9ef6079
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-02 15:06:42 +0800

    Объекты. Часть 1

---
+++ модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 10. Объекты. Часть 1/script.js
@@ -0,0 +1,13 @@
+var pay={
+	Коля : 30000,
+	Петя : 20000,
+	Вася : 25000
+};
+
+console.log(pay.Коля);
+console.log(pay.Петя);
+
+var obj = {a:1, b:2, c:3};
+
+document.write(obj['c'] + '<br>');
+document.write(obj.c);
