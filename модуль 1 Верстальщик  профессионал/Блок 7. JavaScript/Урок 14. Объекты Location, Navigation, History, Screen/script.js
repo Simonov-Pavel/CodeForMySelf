5831189a5921c76bef9ae0d76a221882416855e2
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-03 22:40:44 +0800

    Разбор URL

--- модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 14. Объекты Location, Navigation, History, Screen/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 14. Объекты Location, Navigation, History, Screen/script.js
@@ -1,9 +1,17 @@
-window.onload = function(){
-	window.location = 'http://google.com/index.php?param1=sometext&m2=sometext2 ';
-	function urlArgum(){
-		var args={};
-		var query = window.location.search.substring(1);
-		alert(query);
-	}
-	urlArgum();
+function urlArg(){
+	var args = {};
+	var url = 'http://google.com/index.php?param1=sometext&m2=sometext2 ';
+	var query = url.split('?');
+	query = query[1];
+	var parts = query.split('&');
+	for(var i = 0; i<parts.length; i++){
+		var pos = parts[i].indexOf('=');
+		if(pos == -1){ continue;}
+		var name = parts[i].substring(0,pos);
+		var value = parts[i].substring(pos+1);
+		args[name] = value;
+	} 
+	return args;
 }
+var obj = urlArg();
+console.log(obj);
