a75f8648f6d11f0c1ebc9f04063c3a6964179710
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-03 12:23:23 +0800

    popup js

--- модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 12. Объект JavaScript Window. Часть 1/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 12. Объект JavaScript Window. Часть 1/script.js
@@ -1,1 +1,39 @@
+var show = document.getElementById('show');
+var over = document.getElementById('overflow');
+var close = document.getElementById('popup__close');
+var tIn, tOut;
+show.onclick = popup;
+close.onclick = popupClose;
+function popup(){
+	over.style.display = 'flex';
+	popupIn(1);
+}
+function popupClose(){
+	popupOut(0);
+}
+function popupIn(x){
+	var op = (over.style.opacity) ? parseFloat(over.style.opacity) : 0;
+	if(op < x){
+		clearInterval(tOut);
+		op += 0.05;
+		over.style.opacity = op;
+		tIn = setTimeout(function(){
+			popupIn(x);
+		},100);
+	}
+}
+function popupOut(x){
+	var op = (over.style.opacity) ? parseFloat(over.style.opacity) : 1;
+	if(op > x){
+		clearInterval(tIn);
+		op -= 0.05;
+		over.style.opacity = op;
+		tOut = setTimeout(function(){
+			popupOut(x);
+		},100);
+	}
+	if(over.style.opacity == 0){
+		over.style.display = 'none'
+	}
+}
 
