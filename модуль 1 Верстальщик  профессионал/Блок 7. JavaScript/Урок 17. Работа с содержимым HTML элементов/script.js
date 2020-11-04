4b3587803c3137adaef3f1edad9d266254fd7a9a
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-04 16:30:09 +0800

    Работа с содержимым HTML элементов

--- модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 17. Работа с содержимым HTML элементов/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 17. Работа с содержимым HTML элементов/script.js
@@ -1,5 +1,21 @@
 var arr = document.querySelectorAll('a');
 
 for(i=0; i<arr.length; i++){
-	arr[i].innerHTML='Пункт'+(i+1);
+	//arr[i].innerHTML='Пункт'+(i+1);
+	//console.log(arr[i].outerHTML = '<h2>' + arr[i].outerHTML + '</h2>');
+//textContent поддерживается в IE (innerText)
+	//arr[i].textContent = 'Ссылка' + (i+1);
+	textCont(arr[i], 'Ссылка' + (i+1));
 }
+	function textCont(element, value){
+		var content = element.textContent;
+
+		if(value === undefined){
+			if(content !== undefined) return content;
+			else return element.innerText;
+		}
+		else{
+			if(content !== undefined) element.textContent = value;
+			else element.innerText = value;
+		}
+	} 
