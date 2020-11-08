d2f50e0b5fd93dac210a5f9ec217d426726cf928
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-08 23:31:08 +0800

    Обработка событий в JavaScript

--- модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 19. Обработка событий в JavaScript/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 19. Обработка событий в JavaScript/script.js
@@ -20,3 +20,34 @@ for (var i = 0; i < img.length; i++) {
 function src() {
 	text.innerHTML = this.getAttribute('src');
 }
+
+var batton = document.getElementsByTagName('button');
+
+for(var i = 0; i < batton.length; i++){
+	if(batton[i].addEventListener) batton[i].addEventListener('click',addRed, false);
+	else if(batton[i].attachEvent) batton[i].attachEvent('onclick', addRed);
+}
+function addRed(){
+	this.style.color = 'red';
+	if(this.addEventListener){
+		this.removeEventListener('click', addRed, false);	
+		this.addEventListener('click', addGreen, false);	
+	}
+	else if(this.attachEvent){
+		this.attachEvent('onclick', addRed, false);	
+		this.detachEvent('onclick', addGreen, false);
+	}
+}
+function addGreen(){
+	this.style.color = 'green';
+	if(this.addEventListener){
+		this.removeEventListener('click', addGreen, false);	
+		this.addEventListener('click', addRed, false);	
+	}
+	else if(this.attachEvent){
+		this.attachEvent('onclick', addGreen, false);	
+		this.detachEvent('onclick', addRed, false);
+	}
+
+
+}
