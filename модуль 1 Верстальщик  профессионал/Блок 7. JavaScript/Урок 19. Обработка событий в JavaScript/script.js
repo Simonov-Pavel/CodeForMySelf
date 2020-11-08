6320c804c713ba5994cfd494a2ff8467d2699274
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-08 22:34:53 +0800

    Обработка событий в JavaScript

--- модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 19. Обработка событий в JavaScript/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 19. Обработка событий в JavaScript/script.js
@@ -1,11 +1,22 @@
-window.onload = function(){
-	var text = document.getElementById('text');
-	var form = document.querySelectorAll('input');
-	console.log(form);
 
-	for(var i = 0; i <form.length; i++){
-		form[i].onblur = (text.innerHTML = 'hhh - '+ i);
-	}
-
+var text = document.getElementById('text');
+var input = document.getElementsByTagName('input');
+var img = document.getElementsByTagName('img');
 	
+for (var i = 0; i < input.length; i++) {
+	if(input[i].addEventListener)  input[i].addEventListener('blur', func, false);
+	else if(input[i].attachEvent)  input[i].attachEvent('onblur',func);
+}
+
+function func() {
+	text.innerHTML = this.value;
+}
+
+
+for (var i = 0; i < img.length; i++) {
+	img[i].onclick = src;
+}
+
+function src() {
+	text.innerHTML = this.getAttribute('src');
 }
