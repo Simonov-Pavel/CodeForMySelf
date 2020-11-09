7626cc5ab0c83c859f736390707a46a91566ad98
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-09 15:31:17 +0800

    Аргументы обработчиков событий

--- модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 20. Аргументы обработчиков событий. Отмена событий/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 20. Аргументы обработчиков событий. Отмена событий/script.js
@@ -1,51 +1,19 @@
-
-var text = document.getElementById('text');
-var input = document.getElementsByTagName('input');
-var img = document.getElementsByTagName('img');
-	
-for (var i = 0; i < input.length; i++) {
-	if(input[i].addEventListener)  input[i].addEventListener('blur', func, false);
-	else if(input[i].attachEvent)  input[i].attachEvent('onblur',func);
+var link = document.getElementsByTagName('a');
+for(var i = 0; i<link.length; i++){
+	addEvent(link[i], 'mouseover', addHref);
 }
-
-function func() {
-	text.innerHTML = this.value;
-}
-
-
-for (var i = 0; i < img.length; i++) {
-	img[i].onclick = src;
-}
-
-function src() {
-	text.innerHTML = this.getAttribute('src');
-}
-
-var batton = document.getElementsByTagName('button');
-
 function addEvent(target, type, func){
 	if(target.addEventListener) target.addEventListener(type, func, false);
-	else if(target.attachEvent) target.attachEvent('on'+type, function(e){
-		return func.call(target, e);
-	});
+	else target.attachEvent('on'+type, func);
 }
 function removeEvent(target, type, func){
 	if(target.removeEventListener) target.removeEventListener(type, func, false);
-	else if(target.detachEvent) target.detachEvent('on'+type, function(e){
+	else target.detachEvent('on'+type, function(e){
 		return func.call(target, e);
 	});
 }
-
-for(var i = 0; i < batton.length; i++){
-	addEvent(batton[i], 'click', addRed);
-}
-function addRed(){
-	this.style.color = 'red';
-	removeEvent(this, 'click', addRed);
-	addEvent(this, 'click', addGreen);
-}
-function addGreen(){
-	this.style.color = 'green';
-	removeEvent(this, 'click', addGreen);
-	addEvent(this, 'click', addRed);
+function addHref(){
+		var href = this.getAttribute('href')
+		this.innerHTML = this.innerHTML + ' ( '+href+' )';
+		removeEvent(this, 'mouseover', addHref);
 }
