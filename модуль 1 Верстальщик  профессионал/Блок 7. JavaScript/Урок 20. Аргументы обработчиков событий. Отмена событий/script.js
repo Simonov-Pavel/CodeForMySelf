bf6d3952fd177442e0f0ab93dc855bf1e88bbca3
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-09 16:12:43 +0800

    Аргументы обработчиков событий

--- модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 20. Аргументы обработчиков событий. Отмена событий/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 20. Аргументы обработчиков событий. Отмена событий/script.js
@@ -1,6 +1,7 @@
 var link = document.getElementsByTagName('a');
 for(var i = 0; i<link.length; i++){
 	addEvent(link[i], 'mouseover', addHref);
+	addEvent(link[i], 'click', removeHref);
 }
 function addEvent(target, type, func){
 	if(target.addEventListener) target.addEventListener(type, func, false);
@@ -12,8 +13,17 @@ function removeEvent(target, type, func){
 		return func.call(target, e);
 	});
 }
+var href;
 function addHref(){
-		var href = this.getAttribute('href')
+		href = this.getAttribute('href')
 		this.innerHTML = this.innerHTML + ' ( '+href+' )';
 		removeEvent(this, 'mouseover', addHref);
+}
+function removeHref(e){
+	e = e || window.event;
+	if(e.preventDefault) e.preventDefault();
+	else if(e.returnValue) e.returnValue = false;
+	else false;
+	href = ' ( '+href+ ' )';
+	this.innerHTML = this.innerHTML.replace(href, '');
 }
