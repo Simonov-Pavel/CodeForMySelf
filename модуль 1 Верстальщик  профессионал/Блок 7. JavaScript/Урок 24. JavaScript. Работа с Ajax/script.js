4380e3e70e6b8f8f522372190fe6e58387d43a80
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-12 19:59:02 +0800

    События клавиатуры и элементов форм

--- модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 24. JavaScript. Работа с Ajax/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 7. JavaScript/Урок 24. JavaScript. Работа с Ajax/script.js
@@ -4,30 +4,25 @@ function addEvent(target, type, func){
 	else false;
 }
 var batton = document.getElementById('button');
-addEvent(button,'click', formAjax);
+addEvent(batton,'click', formAjax);
 
 function formAjax(e){
 	e = e || window.event;
-	//e.preventDefault();
-	//if(e.returnValue) e.returnValue = false;
-	//else return false;
-	
 	var formDate = {
 				name: document.querySelector('input[name="name"]').value,
 				email: document.querySelector('input[name="email"]').value
 				}
 	var request = new XMLHttpRequest();
-	request.open('POST','server.php',true);
-	request.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-	request.readystatechange = function(){
+	request.open("POST",'server.php');
+	request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+
+	request.onreadystatechange = function(){ 
 		if(request.readyState === 4 && request.status === 200){
-			document.getElementById('firstForm').innerHTML = '<h2>Спасибо</h2>';
+			document.getElementById('firstForm').innerHTML = request.responseText;
 		}
-	request.send('name='+formDate.name+'&email='+formDate.email);
-	console.log(request.responseText);
 	}
 
-	//e.preventDefault();
-	//if(e.returnValue) e.returnValue = false;
-	//return false;
+	request.send('name='+formDate.name+'&email='+formDate.email);
+	e.preventDefault();
+
 }
