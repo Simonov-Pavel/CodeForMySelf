7de20bafe6129f36bca719a00f7a8fceb43dc4c6
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-14 14:23:36 +0800

    jQuery селекторы

--- модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 2. jQuery селекторы/js/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 2. jQuery селекторы/js/script.js
@@ -1,4 +1,17 @@
 
 jQuery(function($) {
-	
+	//Выбрать элементы на странице по имени тега
+	//$('form').css({'border':'1px solid red'});
+	//Выбрать элементы на странице по имени класса
+	//$('.container').css({'border':'1px solid red'});
+	//Выбрать элементы на странице по идентификатору,
+	//$('#contact-form').css({'border':'1px solid red'});
+	//выбрать несколько элементов
+	//$('.grid_12,.grid_7,#contact-form').css({'border':'1px solid red'});
+	//выбрать элементы вложенные в некоторый родительский блок.
+	//$('.grid_12 > h1').css({'border':'1px solid red'});
+	//$('.container > div').css({'border':'1px solid red'});
+	//$('h1 + nav').css({'border':'1px solid red'});
+	//$('header ~ fieldset').css({'border':'1px solid red'});
+	$('.container > div > div').css({'border':'1px solid red'});
 });
