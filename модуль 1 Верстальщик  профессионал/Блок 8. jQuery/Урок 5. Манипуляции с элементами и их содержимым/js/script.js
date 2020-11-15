ca9eb36227eddf350d7bc255dbb108603ca084cb
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-15 16:29:32 +0800

    Манипуляции с элементами и их содержимым

--- модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 5. Манипуляции с элементами и их содержимым/js/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 5. Манипуляции с элементами и их содержимым/js/script.js
@@ -1,1 +1,27 @@
+ 
+			//Добавить html содержимое в конец произвольного блока.
+ var count = 0;
+function  func(){
+	count++;
+	var header = $('.grid_12 header h2 span');
+	
+	var res = header.text().substr(8);
+	if(res){
+		text = header.text().replace(res, ' - result '+count);
+		header = header.html(text);
+	}
+	else 
+		header = header.append(' - result '+count);
 
+			//Привести к верхнему регистру все четные элементы произвольного списка
+$('.sf-menu li a').text(function(index,oldValue){
+	if(count%2 == 0){
+		if(index%2 == 0){
+			return oldValue.toUpperCase();
+		}
+	}
+		else return oldValue.toLowerCase();
+});
+
+
+}
