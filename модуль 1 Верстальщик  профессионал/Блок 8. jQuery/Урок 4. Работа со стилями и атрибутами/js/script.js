1a01b65b004aeef74cb04ddb8ef4b85e4784ed60
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-15 11:21:41 +0800

    Работа со стилями и атрибутами

--- модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 4. Работа со стилями и атрибутами/js/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 4. Работа со стилями и атрибутами/js/script.js
@@ -6,7 +6,16 @@ function func(){
 	$('img').css('border', '2px solid red');
 
 		//Изменить значение атрибута src для изображения
-	img.removeAttr('src');
-	alert(img.attr('src'));
+	if(img.attr('src') == 'images/index-1_img-1.jpg'){
+		img.attr('src', 'images/kot.jpg');
+	}
+	else img.attr('src', 'images/index-1_img-1.jpg');
+
+		//Добавить к заголовку H1 класс title
+	var H1 = $('.grid_12 > h1');
+	if(H1.hasClass('title')){
+		H1.removeClass('title');
+	}
+	else H1.addClass('title');
 
 }
