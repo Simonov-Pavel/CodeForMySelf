0532e0dc5d87b6cb5274945ee073d4544b596942
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-15 19:58:10 +0800

    Методы анимационных эффектов

--- модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 6. Методы анимационных эффектов/js/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 6. Методы анимационных эффектов/js/script.js
@@ -1,27 +1,13 @@
  
-			//Добавить html содержимое в конец произвольного блока.
- var count = 0;
-function  func(){
-	count++;
-	var header = $('.grid_12 header h2 span');
-	
-	var res = header.text().substr(8);
-	if(res){
-		text = header.text().replace(res, ' - result '+count);
-		header = header.html(text);
-	}
-	else 
-		header = header.append(' - result '+count);
+			//Анимационно изменить размер шрифта заголовка страницы за 2 секунды.
 
-			//Привести к верхнему регистру все четные элементы произвольного списка
-$('.sf-menu li a').text(function(index,oldValue){
-	if(count%2 == 0){
-		if(index%2 == 0){
-			return oldValue.toUpperCase();
-		}
-	}
-		else return oldValue.toLowerCase();
-});
 
+function func(){
+	$('.grid_12 header h2')
+		.animate({'fontSize': '200px'},2000)
+		.animate({'opacity': 0},2000)
+		.animate({'fontSize': '0px'},50)
+		.animate({'opacity': 1},50)
+		.animate({'fontSize': '72px'},2000);
 
 }
