4e12ceb3cdea7640ab46862244b72af562b6bf83
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-21 21:38:41 +0800

    jQuery события. Часть 2

--- модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 8. jQuery события. Часть 2/js/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 8. jQuery события. Часть 2/js/script.js
@@ -1,2 +1,11 @@
+function func(){
+	$('.grid_12 header h2').append('<div class="btnDiv" style="width:100px;height:100px;background:red;margin:10px;display:inline-block"></div>');
+}
 
+$('.btnDiv').on('click', function(){
+	$('.btnDiv').remove();
+});
 
+$(document).on('click','.btnDiv',function(){
+       $(this).remove();
+});
