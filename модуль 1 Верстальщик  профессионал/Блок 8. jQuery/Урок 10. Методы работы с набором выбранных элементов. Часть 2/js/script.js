38163dd79be357a81ba5010181c3f8a2c3ac5320
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-22 16:38:06 +0800

    Методы работы с набором выбранных элементов. Часть 2

--- модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 10. Методы работы с набором выбранных элементов. Часть 2/js/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 10. Методы работы с набором выбранных элементов. Часть 2/js/script.js
@@ -11,3 +11,50 @@
 		//Методы обработки элементов выборки
 //.each() - Вызывает заданую функцию для каждого элемента набора
 //.map() - Вызывает заданую функцию для каждого элемента набора, а в итоге создает новый набор, составленныйиз значений, возвращенных этой функциуй
+
+jQuery(function($){
+
+	//$('li').eq(2).css({'border':'1px solid black'});
+
+	//$('div').filter('.address').css({'border':'1px solid black'});
+	/*$('li').filter(function(index){
+		//return $(this).attr('class') == 'current';
+		return index%2 == 0;
+	}).css({'border':'1px solid black'});*/
+
+	//$('div').first().css({'border':'1px solid green'});
+
+	//$('div').last().css({'border':'1px solid green'});
+
+	//$('div').has('h1, .row').css({'border':'1px solid green'});
+
+	/*if($('div').is('.grid_7')){
+		alert('yes');
+	}*/
+	
+	/*$('span').parent().not(function(index){
+		return $(this).is('h1');
+	}).css({'border':'1px solid red'});*/
+
+	// $('li').slice(0,3).css({'border':'1px solid black'}); 
+
+			//Выбрать все элементы списка и их текстовое содержимое записать в массив (каждый отдельный текст списка в отдельную ячейку)
+			
+	/*var arr = [];
+	$('li').each(function(index,elem){
+		arr.push($(elem).text());
+	});
+	alert(arr);*/
+
+	/*var arr = [];
+	$('li').map(function(index,elem){
+		arr.push($(elem).text());
+	});
+	alert(arr);*/
+
+	var arr = $('li').map(function(index,elem){
+		return $(elem).text();
+	});
+	alert(arr.get());
+
+});
