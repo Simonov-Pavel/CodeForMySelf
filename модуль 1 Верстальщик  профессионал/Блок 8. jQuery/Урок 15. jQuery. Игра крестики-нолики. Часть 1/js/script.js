e4c1c9c60b33e7410cb9b1f1da0030edda63ba0b
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-11-28 06:51:17 +0800

    jquery

--- модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 15. jQuery. Игра крестики-нолики. Часть 1/js/script.js
+++ модуль 1 Верстальщик  профессионал/Блок 8. jQuery/Урок 15. jQuery. Игра крестики-нолики. Часть 1/js/script.js
@@ -1,6 +1,17 @@
 jQuery(function($){
 
+	var player = 'x';
 
-	
+	$('.wrap').on('click', ".cell:not('.cell-x, .cell-o')", oneStep);
+
+	function oneStep(event){
+		var $cell = $(event.currentTarget);
+
+		$cell.addClass('cell-' + player + ' offset-' + player);
+
+		if(player === 'x') player = 'o';
+		else player = 'x';
+	}
+
 });
 
