ed52db72c639e0a2cf133d0071912ce125575c0e
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2020-12-06 11:45:40 +0800

    Директивы @for, @each и @while

---
+++ модуль 1 Верстальщик  профессионал/Блок 9. Sass/Урок 11. Директивы @for, @each и @while/SASS/gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const browseSync = require('browser-sync').create();
+const sourceMaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function(){
+    return gulp.src('scss/style.scss')
+        .pipe(plumber())
+        .pipe(sourceMaps.init())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            overrideBrowserlist: ["last 5 versions"],
+            cascade: true
+        }))
+        .pipe(sourceMaps.write())
+        .pipe(gulp.dest('build/css'))
+        .pipe(browseSync.reload({stream: true}));
+});
+gulp.task('html', function(){
+    return gulp.src('*.html')
+        .pipe(gulp.dest('build'))
+        .pipe(browseSync.reload({stream: true}));
+});
+
+gulp.task("serve", gulp.series('html', 'sass', function(){
+    browseSync.init({
+        server: "build"
+    });
+    gulp.watch("scss/**/*.scss", gulp.parallel("sass"));
+    gulp.watch("*.html", gulp.parallel("html"));
+}));
