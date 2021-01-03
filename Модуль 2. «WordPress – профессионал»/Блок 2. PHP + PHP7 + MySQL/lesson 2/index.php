c2e3690808e42d1b26b25304d7d9a24a3ebc2462
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2021-01-03 18:18:42 +0800

    Синтаксис PHP. Основы синтаксиса языка PHP

---
+++ Модуль 2. «WordPress – профессионал»/Блок 2. PHP + PHP7 + MySQL/lesson 2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Урок 2</title>
+</head>
+<body>
+<!-- Coment -->
+	<h1>Синтаксис PHP. Основы синтаксиса языка PHP</h1>
+	<?php
+	//Однострочный комментарий
+	/* Многострочный
+	комментарий */
+		echo '<p>Hollo word!</p>'; //Вывод текста на экран
+		echo __DIR__; die;
+	?>
+</body>
+</html>
