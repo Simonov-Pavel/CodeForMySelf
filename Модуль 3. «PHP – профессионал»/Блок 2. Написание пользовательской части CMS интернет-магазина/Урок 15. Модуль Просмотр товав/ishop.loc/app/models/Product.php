613e357cc5e10e35c863b5b56db908c6d8fd17c6
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2021-04-06 21:50:45 +0800

    Урок 15. Карточка товара. Модуль Просмотренные товары

---
+++ Модуль 3. «PHP – профессионал»/Блок 2. Написание пользовательской части CMS интернет-магазина/Урок 15. Модуль Просмотр товав/ishop.loc/app/models/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace app\models;
+
+
+class Product extends AppModel
+{
+    public function setRecentlyViewed($id)
+    {
+        $resentlyViewed = $this->getAllRecentlyViewed();
+        if (!$resentlyViewed) {
+            setcookie('resentlyViewed', $id, time() + 3600 * 24, '/');
+        } else {
+            $resentlyViewed = explode('.', $resentlyViewed);
+            if (!in_array($id, $resentlyViewed)) {
+                $resentlyViewed[] = $id;
+                $resentlyViewed = implode('.', $resentlyViewed);
+                setcookie('resentlyViewed', $resentlyViewed, time() + 3600 * 24, '/');
+            }
+        }
+    }
+
+    public function getRecentlyViewed()
+    {
+        if (!empty($_COOKIE['resentlyViewed'])) {
+            $resentlyViewed = $_COOKIE['resentlyViewed'];
+            $resentlyViewed = explode('.', $resentlyViewed);
+            return array_slice($resentlyViewed, -3);
+        }
+        return false;
+    }
+
+    public function getAllRecentlyViewed()
+    {
+        if (!empty($_COOKIE['resentlyViewed'])) {
+            return $_COOKIE['resentlyViewed'];
+        }
+        return false;
+    }
+}
