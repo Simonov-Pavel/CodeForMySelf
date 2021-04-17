8cf42d8f9737ba081a40421bda7865000a22a21d
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2021-04-18 03:51:50 +0800

    Урок 21. Корзина. Часть4

---
+++ Модуль 3. «PHP – профессионал»/Блок 2. Написание пользовательской части CMS интернет-магазина/Урок 21. Корзина. Часть4/ishop.loc/vendor/ishop/core/libs/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function debag($arr)
+{
+    echo '<pre>' . print_r($arr, true) . '</pre>';
+}
+
+function redirect($http = false)
+{
+    if ($http) {
+        $redirect = $http;
+    } else {
+        $redirect = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PATH;
+    }
+    header("Location: $redirect");
+    exit;
+}
+
