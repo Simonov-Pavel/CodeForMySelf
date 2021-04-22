75c3b3e8687edea3b749785ad90968f295c52311
Simonov-Pavel <68182832+Simonov-Pavel@users.noreply.github.com>
2021-04-22 20:09:49 +0800

    Урок 29. Регистрация. Часть 3

---
+++ Модуль 3. «PHP – профессионал»/Блок 2. Написание пользовательской части CMS интернет-магазина/Урок 29. Регистрация. Часть 3/ishop.loc/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'gabordemooij/redbean' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '45332d4b595695ee747965bc9bcdb5b5b1f76633',
+    ),
+    'vlucas/valitron' => 
+    array (
+      'pretty_version' => 'v1.4.9',
+      'version' => '1.4.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '81515dcc951e1f636a1a18ece2f4154dfa123438',
+    ),
+  ),
+);
